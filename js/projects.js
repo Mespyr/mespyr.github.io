ed4d00e2b1b91ffbcccf29bd92edf1c22b79e21c
Mespyr <senukcw@gmail.com>
2022-10-02 17:57:18 -0400

    checking to see if some of my javascript code works

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,8 @@
 fetch('./data/projects.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+	.then((json) => {
+
+	console.log("Checking javascript syntax to see if this works")
+	console.log(json)
+
+});
