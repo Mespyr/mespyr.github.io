8a1850a4d6c52e69580e833270b4dfeaaed7fddf
Mespyr <senukcw@gmail.com>
2022-10-03 22:19:06 -0400

    added intro page to website and changed color again

--- js/projects.js
+++ js/projects.js
@@ -5,12 +5,12 @@ fetch('./data/projects.json')
 	json.forEach(function(proj, idx, arr) {
 		// create html out of data in json
 		let sect = document.createElement('a')
-		sect.className = "container-item"
+		sect.className = "project-container-item"
 		sect.href = proj.link
 		sect.innerHTML = "<h2>" + proj.name + "</h2>" + proj.desc
 
 		// get container and insert 'div' at the end of it
-		let container = document.getElementById("container")
+		let container = document.getElementById("project-container")
 		container.insertBefore(sect, null)
 	})
 });
