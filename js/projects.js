3d15879a6320bb96193f3581ae6cc756ee5519e0
Mespyr <senukcw@gmail.com>
2022-10-18 09:42:55 -0400

    redesign website to look a lot better and be cooler and stuff

--- js/projects.js
+++ js/projects.js
@@ -5,12 +5,12 @@ fetch('./data/projects.json')
 	json.forEach(function(proj, idx, arr) {
 		// create html out of data in json
 		let sect = document.createElement('a')
-		sect.className = "project-container-item"
+		sect.className = "text-card"
 		sect.href = proj.link
 		sect.innerHTML = "<h2>" + proj.name + "</h2>" + proj.desc
 
 		// get container and insert 'div' at the end of it
-		let container = document.getElementById("project-container")
+		let container = document.getElementById("card-container")
 		container.insertBefore(sect, null)
 	})
 });
