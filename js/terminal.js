decc877c658bc21b6899eb26bc3cca196d25ad3c
Mespyr <senukcw@gmail.com>
2023-01-18 21:00:23 -0500

    cool stuff

--- js/terminal.js
+++ js/terminal.js
@@ -14,14 +14,14 @@ document.addEventListener("keydown", ({_}) => {
 function init_terminal()
 {
 	/* make window draggable */
-	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	var mouse_delta_x = 0, mouse_delta_y = 0, mouse_x = 0, mouse_y = 0;
 	let _window = document.getElementById("window");
 	document.getElementById("titlebar").onmousedown = ({e}) => {
 		e = e || window.event;
 		e.preventDefault();
 		// get the mouse cursor position at startup:
-		pos3 = e.clientX;
-		pos4 = e.clientY;
+		mouse_x = e.clientX;
+		mouse_y = e.clientY;
 		document.onmouseup = () => {
 			// stop moving when mouse button is released:
 			document.onmouseup = null;
@@ -32,66 +32,66 @@ function init_terminal()
 			r = r || window.event;
 			r.preventDefault();
 			// calculate the new cursor position:
-			pos1 = pos3 - r.clientX;
-			pos2 = pos4 - r.clientY;
-			pos3 = r.clientX;
-			pos4 = r.clientY;
+			mouse_delta_x = mouse_x - r.clientX;
+			mouse_delta_y = mouse_y - r.clientY;
+			mouse_x = r.clientX;
+			mouse_y = r.clientY;
 			// set the element's new position:
-			_window.style.top = (_window.offsetTop - pos2) + "px";
-			_window.style.left = (_window.offsetLeft - pos1) + "px";
+			_window.style.top = (_window.offsetTop - mouse_delta_y) + "px";
+			_window.style.left = (_window.offsetLeft - mouse_delta_x) + "px";
 		}
 	}
 
 	/* display terminal messages */
 	terminal.appendChild(create_line("Type <span class='cyan'>'help'</span> to get a list of commands."));
+	requestAnimationFrame(terminal_loop);
+}
 
-	var waiting_for_input = false;
-	var current_prompt = null;
-
-	function loop()
+// main terminal prompt loop
+var waiting_for_input = false;
+var current_prompt = null;
+function terminal_loop()
+{
+	if (!waiting_for_input)
 	{
-		if (!waiting_for_input)
+		// destroy old prompt and replace it with regular line
+		if (current_prompt != null)
 		{
-			// destroy old prompt and replace it with regular line
-			if (current_prompt != null)
-			{
-				let text = current_prompt.value;
-				current_prompt.parentNode.parentNode.removeChild(current_prompt.parentNode);
-				terminal.appendChild(create_line("<span class='pink'>>>></span> " + text));
+			let text = current_prompt.value;
+			current_prompt.parentNode.parentNode.removeChild(current_prompt.parentNode);
+			terminal.appendChild(create_line("<span class='pink'>>>></span> " + text));
 
-				// parse prompt
-				if (text === "");
-				else if (text === "help")
-				{
+			// parse prompt
+			switch (text)
+			{
+				case "":
+					break;
+				case "help":
 					terminal.appendChild(create_line("Hello! Welcome to my website."));
 					terminal.appendChild(create_line("Here's a list of all the runnable commands."));
 					terminal.appendChild(create_line("&nbsp;"));
 					terminal.appendChild(create_line("&nbsp;&nbsp;&nbsp;&nbsp;help&nbsp;&nbsp;&nbsp; display this message"));
 					terminal.appendChild(create_line("&nbsp;&nbsp;&nbsp;&nbsp;clear&nbsp;&nbsp; clear the terminal"));
 					terminal.appendChild(create_line("&nbsp;"));
-				}
-				else if (text === "clear")
-				  terminal.textContent = '';
-				else
+					break;
+				case "clear":
+					terminal.textContent = '';
+					break
+				default:
 					terminal.appendChild(create_line(text + ": command not found "));
 			}
-			// create a new prompt
-			terminal.appendChild(create_prompt());
-			terminal.scroll(0, terminal.scrollHeight)
-			current_prompt = document.getElementById("command-prompt");
-			// add an event listener to wait for when you press enter in the prompt
-			current_prompt.addEventListener("keydown", ({key}) => {
-				if (key === "Enter") {
-					waiting_for_input = false;
-				}
-			})
-			current_prompt.focus();
-			// resume waiting for input until enter is pressed
-			waiting_for_input = true;
 		}
-		requestAnimationFrame(loop);
+		terminal.appendChild(create_prompt()); // create a new prompt
+		terminal.scroll(0, terminal.scrollHeight)
+		current_prompt = document.getElementById("command-prompt");
+		// add an event listener to wait for when you press enter in the prompt
+		current_prompt.addEventListener("keydown", ({key}) => {
+			if (key === "Enter") waiting_for_input = false;
+		})
+		current_prompt.focus();
+		waiting_for_input = true; // resume waiting for input until enter is pressed
 	}
-	requestAnimationFrame(loop);
+	requestAnimationFrame(terminal_loop);
 }
 
 function create_line(message)
