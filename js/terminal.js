95e0c19b08706adb4db4d03fa39adf7fc3bf9ff9
Mespyr <senukcw@gmail.com>
2023-01-30 22:03:08 -0500

    cool stuff

--- js/terminal.js
+++ js/terminal.js
@@ -10,7 +10,14 @@ document.addEventListener("keydown", ({_}) => {
 	}
 })
 
-/* init terminal and window start prompt loop */
+const banner = `<pre class='glow'>
+ • ▌ ▄ ·. ▄▄▄ ..▄▄ ·  ▄▄▄· ▄· ▄▌▄▄▄   
+ ·██ ▐███▪▀▄.▀·▐█ ▀. ▐█ ▄█▐█▪██▌▀▄ █· 
+ ▐█ ▌▐▌▐█·▐▀▀▪▄▄▀▀▀█▄ ██▀·▐█▌▐█▪▐▀▀▄  
+ ██ ██▌▐█▌▐█▄▄▌▐█▄▪▐█▐█▪·• ▐█▀·.▐█•█▌ 
+ ▀▀  █▪▀▀▀ ▀▀▀  ▀▀▀▀ .▀     ▀ • .▀  ▀</pre>Type <span class='cyan'>'help'</span> to get a list of commands.`
+
+/* init terminal and window */
 function init_terminal()
 {
 	/* make window draggable */
@@ -51,7 +58,8 @@ function init_terminal()
 	}
 
 	/* display terminal messages */
-	terminal.appendChild(create_line("Type <span class='cyan'>'help'</span> to get a list of commands."));
+
+	terminal.appendChild(create_line(banner));
 	requestAnimationFrame(terminal_loop);
 }
 
@@ -78,9 +86,10 @@ function terminal_loop()
 					terminal.appendChild(create_line("Hello! Welcome to my website."));
 					terminal.appendChild(create_line("Here's a list of all the runnable commands."));
 					terminal.appendChild(create_line("&nbsp;"));
-					terminal.appendChild(create_line("&nbsp;&nbsp;&nbsp;&nbsp;help&nbsp;&nbsp;&nbsp;&nbsp; display this message"));
-					terminal.appendChild(create_line("&nbsp;&nbsp;&nbsp;&nbsp;clear&nbsp;&nbsp;&nbsp; clear the terminal"));
-					terminal.appendChild(create_line("&nbsp;&nbsp;&nbsp;&nbsp;whoami&nbsp;&nbsp; print user info"));
+					terminal.appendChild(create_line("    <span class='cyan'>help</span>       display this message"));
+					terminal.appendChild(create_line("    <span class='cyan'>clear</span>      clear the terminal"));
+					terminal.appendChild(create_line("    <span class='cyan'>whoami</span>     print user info"));
+					terminal.appendChild(create_line("    <span class='cyan'>banner</span>     print the terminal banner"));
 					terminal.appendChild(create_line("&nbsp;"));
 					break;
 				case "clear":
@@ -94,7 +103,12 @@ function terminal_loop()
 					terminal.appendChild(create_line("&nbsp;"));
 					terminal.appendChild(create_line("I also enjoy playing the bari sax"));
 					terminal.appendChild(create_line("and drawing in my free time."));
+					terminal.appendChild(create_line("&nbsp;"));
 					break;
+				case "banner":
+					terminal.appendChild(create_line(banner));
+					break;
+
 				default:
 					terminal.appendChild(create_line(text + ": command not found "));
 			}
@@ -121,7 +135,7 @@ function create_line(message)
 }
 function create_prompt()
 {
-	const elem = create_line("user@mespyr ~&nbsp;<span contenteditable='true' id='command-prompt' type='text'>");
+	const elem = create_line("user@mespyr ~ <span contenteditable='true' id='command-prompt' type='text'>");
 	elem.classList.add("pink");
 	// elem.classList.add("bold");
 	elem.id = "command-prompt-wrapper";
